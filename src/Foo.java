e222220bfbb5d95f5b3ce8798b606d47ad5e9f21
Wei Guangcheng <twksos@qq.com>
2012-12-06 22:39:54 +0800

    auto test show all fails

--- src/Foo.java
+++ src/Foo.java
@@ -4,27 +4,25 @@
  */
 public class Foo {
     public int add(int a, int b) {
-        return a + b;
+        return a - b;
     }
 
     public static void main(String[] args) {
-        foo_add_1_and_2_should_be_3();
-        foo_add_2_and_2_should_be_4();
+        boolean success = true;
+        success = foo_add_1_and_2_should_be_3() && success;
+        success = foo_add_2_and_2_should_be_4() && success;
+        if(!success) System.exit(1);
     }
 
-    private static void foo_add_2_and_2_should_be_4() {
+    private static boolean foo_add_2_and_2_should_be_4() {
         boolean foo_add_2_and_2_should_be_4 = new Foo().add(2, 2) == 4;
-        if (!foo_add_2_and_2_should_be_4) {
-            System.out.println("foo_add_2_and_2_should_be_4 fails!!");
-            System.exit(1);
-        }
+        if (!foo_add_2_and_2_should_be_4) System.out.println("foo_add_2_and_2_should_be_4 fails!!");
+        return foo_add_2_and_2_should_be_4;
     }
 
-    private static void foo_add_1_and_2_should_be_3() {
+    private static boolean foo_add_1_and_2_should_be_3() {
         boolean foo_add_1_and_2_should_be_3 = new Foo().add(1, 2) == 3;
-        if (!foo_add_1_and_2_should_be_3) {
-            System.out.println("foo_add_1_and_2_should_be_3 fails!!");
-            System.exit(1);
-        }
+        if (!foo_add_1_and_2_should_be_3) System.out.println("foo_add_1_and_2_should_be_3 fails!!");
+        return foo_add_1_and_2_should_be_3;
     }
 }
