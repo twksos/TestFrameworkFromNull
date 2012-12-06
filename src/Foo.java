669f1a924a88c61f4f570d24c5070c1e480254d5
Wei Guangcheng <twksos@qq.com>
2012-12-06 21:53:44 +0800

    test_with_eye

--- src/Foo.java
+++ src/Foo.java
@@ -6,4 +6,8 @@ public class Foo {
     public int add (int a, int b) {
         return a + b;
     }
+
+    public static void main(String[] args) {
+        System.out.println(new Foo().add(1,2));
+    }
 }
