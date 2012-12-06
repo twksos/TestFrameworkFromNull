31bb2726f02aa77f6b1a612b38312f8041a6de9f
Wei Guangcheng <twksos@qq.com>
2012-12-06 22:19:51 +0800

    auto test functions fail at 1st

--- src/Foo.java
+++ src/Foo.java
@@ -3,14 +3,27 @@
  * Time: 12-12-6 21:40
  */
 public class Foo {
-    public int add (int a, int b) {
+    public int add(int a, int b) {
         return a + b;
     }
 
     public static void main(String[] args) {
+        foo_add_1_and_2_should_be_3();
+        foo_add_2_and_2_should_be_4();
+    }
+
+    private static void foo_add_2_and_2_should_be_4() {
+        boolean foo_add_2_and_2_should_be_4 = new Foo().add(2, 2) == 4;
+        if (!foo_add_2_and_2_should_be_4) {
+            System.out.println("foo_add_2_and_2_should_be_4 fails!!");
+            System.exit(1);
+        }
+    }
+
+    private static void foo_add_1_and_2_should_be_3() {
         boolean foo_add_1_and_2_should_be_3 = new Foo().add(1, 2) == 3;
-        if(!foo_add_1_and_2_should_be_3){
-        System.out.println("foo_add_1_and_2_should_be_3 fails!!");
+        if (!foo_add_1_and_2_should_be_3) {
+            System.out.println("foo_add_1_and_2_should_be_3 fails!!");
             System.exit(1);
         }
     }
