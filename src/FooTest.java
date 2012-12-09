08bba4e935f1113aefd6d7546141aef534e4e25b
Wei Guangcheng <twksos@qq.com>
2012-12-09 10:10:22 +0800

    auto test use reflection

--- src/FooTest.java
+++ src/FooTest.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Author: Wei Guangcheng
  * Time: 12-12-7 10:06
@@ -6,20 +9,29 @@ public class FooTest {
     public static void main(String[] args) {
         boolean success = true;
         FooTest fooTest = new FooTest();
-        success = fooTest.foo_add_1_and_2_should_be_3() && success;
-        success = fooTest.foo_add_2_and_2_should_be_4() && success;
+        for (Method method : FooTest.class.getMethods()) {
+            if(method.getName().startsWith("test_")){
+            try {
+                success = (Boolean)method.invoke(fooTest) && success;
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }                                        }
+        }
+
         if(!success) System.exit(1);
     }
 
-    private boolean foo_add_2_and_2_should_be_4() {
+    public boolean test_foo_add_2_and_2_should_be_4() {
         boolean foo_add_2_and_2_should_be_4 = new Foo().add(2, 2) == 4;
-        if (!foo_add_2_and_2_should_be_4) System.out.println("foo_add_2_and_2_should_be_4 fails!!");
+        if (!foo_add_2_and_2_should_be_4) System.out.println("test_foo_add_2_and_2_should_be_4 fails!!");
         return foo_add_2_and_2_should_be_4;
     }
 
-    private boolean foo_add_1_and_2_should_be_3() {
+    public boolean test_foo_add_1_and_2_should_be_3() {
         boolean foo_add_1_and_2_should_be_3 = new Foo().add(1, 2) == 3;
-        if (!foo_add_1_and_2_should_be_3) System.out.println("foo_add_1_and_2_should_be_3 fails!!");
+        if (!foo_add_1_and_2_should_be_3) System.out.println("test_foo_add_1_and_2_should_be_3 fails!!");
         return foo_add_1_and_2_should_be_3;
     }
 }
