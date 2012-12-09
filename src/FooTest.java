083d12829712d37a29321f61bfedfade6acf3405
Wei Guangcheng <twksos@qq.com>
2012-12-09 10:40:49 +0800

    auto test use exception

--- src/FooTest.java
+++ src/FooTest.java
@@ -10,28 +10,30 @@ public class FooTest {
         boolean success = true;
         FooTest fooTest = new FooTest();
         for (Method method : FooTest.class.getMethods()) {
-            if(method.getName().startsWith("test_")){
-            try {
-                success = (Boolean)method.invoke(fooTest) && success;
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }                                        }
+            if (method.getName().startsWith("test_")) {
+                try {
+                    method.invoke(fooTest);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (InvocationTargetException e) {
+                    success = false;
+                    Throwable ex = e.getCause();
+                    System.out.println(ex.getMessage());
+                    ex.printStackTrace();
+                }
+            }
         }
 
-        if(!success) System.exit(1);
+        if (!success) System.exit(1);
     }
 
-    public boolean test_foo_add_2_and_2_should_be_4() {
+    public void test_foo_add_2_and_2_should_be_4() throws Exception {
         boolean foo_add_2_and_2_should_be_4 = new Foo().add(2, 2) == 4;
-        if (!foo_add_2_and_2_should_be_4) System.out.println("test_foo_add_2_and_2_should_be_4 fails!!");
-        return foo_add_2_and_2_should_be_4;
+        if (!foo_add_2_and_2_should_be_4) throw new Exception("test_foo_add_2_and_2_should_be_4 fails!!");
     }
 
-    public boolean test_foo_add_1_and_2_should_be_3() {
+    public void test_foo_add_1_and_2_should_be_3() throws Exception {
         boolean foo_add_1_and_2_should_be_3 = new Foo().add(1, 2) == 3;
-        if (!foo_add_1_and_2_should_be_3) System.out.println("test_foo_add_1_and_2_should_be_3 fails!!");
-        return foo_add_1_and_2_should_be_3;
+        if (!foo_add_1_and_2_should_be_3) throw new Exception("test_foo_add_1_and_2_should_be_3 fails!!");
     }
 }
